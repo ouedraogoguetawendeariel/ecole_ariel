4de202bd812e03458d12192d16b64a726d1183ce
Ariel <ouedraogoguetawendeariel@gmail.com>
2026-09-11 16:42:40 +0000

    Fix script source in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: { quality: 75 },
+      jpeg: { quality: 75 },
+      png: { quality: 75 },
+    }),
+  ],
+  base: '/ecole_ariel/',
 })
